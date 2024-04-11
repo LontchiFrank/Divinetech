78818b760858f2bef5bd9e2eb093a2c27de9bf23
LontchiFrank <fodjof6@gmail.com>
2024-04-11 22:19:56 +0100

    change stuff

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,15 @@
 /** @format */
 
 module.exports = {
-	// plugins: {
-	// 	tailwindcss: {},
-	// 	autoprefixer: {},
-	// },
-	plugins: [
-		require("postcss-import"),
-		require("tailwindcss/nesting")(require("postcss-nesting")),
-		require("autoprefixer"),
-		require("tailwindcss"),
-	],
+	plugins: {
+		"tailwindcss/nesting": "postcss-nesting",
+		tailwindcss: {},
+		autoprefixer: {},
+	},
+	// plugins: [
+	// 	require("postcss-import"),
+	// 	require("tailwindcss/nesting")(require("postcss-nesting")),
+	// 	require("autoprefixer"),
+	// 	require("tailwindcss"),
+	// ],
 };
