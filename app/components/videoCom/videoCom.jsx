8d4719ac4ccb1a1b79f2b597898192683526a21a
LontchiFrank <fodjof6@gmail.com>
2024-04-24 12:48:39 +0100

    finished the things

--- app/components/videoCom/videoCom.jsx
+++ app/components/videoCom/videoCom.jsx
@@ -2,13 +2,16 @@
 
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 function VideoCom({ item }) {
 	return (
 		<Link href={item.link}>
 			<div className=" bg-[#EEE0F5] rounded-lg cursor-pointer p-2">
-				<div className="h-[80%] rounded-lg w-full flex justify-center">
-					<img src={item.img} />
+				<div className=" rounded-lg w-full flex justify-center">
+					<div className="w-full ">
+						<img src={item.img} preload="true" />
+					</div>
 				</div>
 				<div className="h-[20%] rounded-md w-full p-3 flex items-center">
 					<span className="text-[#7A24A1]">{item.title}</span>
